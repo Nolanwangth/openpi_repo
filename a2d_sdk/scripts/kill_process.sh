5eb0ea8347b960508183ea9e797ebcace85bf617
nolan <nolanwangth@gmail.com>
2026-03-19 07:49:58 +0800

    稳定版

--- a2d_sdk/scripts/kill_process.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# argv[1] is the process name
-if [ -z "$1" ]; then
-    echo "Usage: $0 <process_name>"
-    exit 1
-fi
-
-ps aux | grep $1 | grep -v grep | awk '{print $2}' | xargs kill -9
